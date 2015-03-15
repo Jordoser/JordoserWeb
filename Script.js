c7cec47f7f4714df1aa7498c73be3c501b516c8e
Jordoser <jmservic@ualberta.ca>
2015-03-15 15:01:00 -0600

    Initial Commit
    Adding basic html/java script/css

---
+++ Script.js
@@ -0,0 +1,9 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Jordan Service. All rights reserved.</p>";
+
+document.getElementById("nav").innerHTML =
+"<ul id='menu'>"+
+"<li><a href='Home.html'>Home</a></li>"+
+"<li><a href='Projects.html'>Projects</a></li>"+
+"<li><a href='CV.html'>CV</a></li>"+
+"</ul>";
