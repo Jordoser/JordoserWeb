dc0a660cfe7a57b42984dab68ad9581ee8b9841e
jordoser <jmservic@ualberta.ca>
2015-03-16 14:13:35 -0600

    removed old files

--- Script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " Jordan Service. All rights reserved.</p>";
-
-document.getElementById("nav").innerHTML =
-"<ul id='menu'>"+
-"<li><a href='Home.html'>Home</a></li>"+
-"<li><a href='Projects.html'>Projects</a></li>"+
-"<li><a href='CV.html'>CV</a></li>"+
-"</ul>";
