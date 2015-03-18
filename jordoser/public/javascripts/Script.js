3525f321447eff7734a2b270e8efb0c94731559d
jordoser <jmservic@ualberta.ca>
2015-03-17 18:14:37 -0600

    Changed Layout, added projects and profile images, add projects to main page and simplified nav bar

--- jordoser/public/javascripts/Script.js
+++
@@ -1,8 +0,0 @@
-
-
-document.getElementById("nav").innerHTML =
-"<ul id='menu'>"+
-"<li><a href='Home.html'>Home</a></li>"+
-"<li><a href='Projects.html'>Projects</a></li>"+
-"<li><a href='/images/JordanServiceCV.pdf'>CV</a></li>"+
-"</ul>";
--- jordoser/routes/index.js
+++ jordoser/routes/index.js
@@ -6,12 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/Contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
 router.get('/facebook', function(req, res) {
     res.redirect('https://www.facebook.com/jordan.service.9');
 });
 
 router.get('/git', function(req, res){
-	res.redirect('https://github.com/Jordoser');
+	res.redirect('https://github.com/Jordoser/Tonight401');
 });
 
 router.get('/twitter', function(req, res){
