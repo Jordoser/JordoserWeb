eca5ba3c0f7cc419f6bc8838499523c198d728a7
jordoser <jmservic@ualberta.ca>
2015-03-17 18:43:51 -0600

    Added a minimum page width to fix formatting issues

---
+++ jordoser/public/javascripts/Script.js
@@ -0,0 +1,15 @@
+function getWidth() {
+  if (self.innerHeight) {
+    return self.innerWidth;
+  }
+
+  if (document.documentElement && document.documentElement.clientHeight) {
+    return document.documentElement.clientWidth;
+  }
+
+  if (document.body) {
+    return document.body.clientWidth;
+}
+
+var width = getWidth();
+document.getElementById("Block2").innerHTML = "p ==",width;
